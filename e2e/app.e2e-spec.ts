fda0af0bbf6ca619831ce5d60e82388f8bc3ff13
hkarom <hfikri@hotmail.fr>
2017-11-10 19:51:52 +0100

    Modif test de base invalide

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,6 @@ describe('pgp App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Welcome to app!');
+    expect(page.getParagraphText()).toEqual('Welcome to PGP 2017!');
   });
 });
