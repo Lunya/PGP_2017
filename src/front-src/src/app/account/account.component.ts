7b9f985c7a739109d28088186d32ae66c9004a61
GaelTM1AL <gaeltrottier@gmail.com>
2017-11-26 20:34:10 +0100

    Account

--- src/front-src/src/app/account/account.component.ts
+++ src/front-src/src/app/account/account.component.ts
@@ -1,16 +1,55 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from 'ng2-validation';
+
+const url = 'http://localhost:3000/api/account';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css'],
-  encapsulation: ViewEncapsulation.None
+  styleUrls: ['./account.component.css']
 })
 export class AccountComponent implements OnInit {
+  private loading = false;
+  private signupForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private http: HttpClient,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    const password = new FormControl(null, [Validators.required, Validators.minLength(8)]);
+    const verifPassword = new FormControl(null, [Validators.required, CustomValidators.equalTo(password)]);
+    this.signupForm = this.fb.group({
+      email: [null, [Validators.required, CustomValidators.email]],
+      name: [null, [Validators.required, Validators.minLength(2)]],
+      password: password,
+      verifPassword: verifPassword
+    });
   }
 
-}
+  ngOnInit() {}
+
+  ngCancel(){
+    this.router.navigate(['/home']);
+  }
+
+  ngOnSubmit() {
+    this.loading = true;
+    this.http.post(url, this.signupForm.value)
+      .subscribe((result: any) => {
+        if (result.error)
+          this.loading = false;
+        else {
+          this.router.navigate(['/home'])
+            .catch(reason => console.log(reason));
+        }
+      }, err => {
+        console.log(err);
+        this.loading = false;
+      });
+
+  }
+ }
