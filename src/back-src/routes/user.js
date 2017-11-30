37e0c81412c3ba76b4e82a4845bd7e5e72b3cd7b
GaelTM1AL <gaeltrottier@gmail.com>
2017-11-30 17:59:51 +0100

    Account + User

--- src/back-src/routes/user.js
+++ src/back-src/routes/user.js
@@ -4,6 +4,7 @@ let bcrypt = require('bcryptjs');
 let bd = require('../databaseConnect');
 
 let router = express.Router();
+const saltRounds = 8;
 
 function treatment(errorStatus, response, values,  rows) {
 	if(errorStatus) response.status(400).send(errorStatus);
@@ -18,7 +19,7 @@ function treatment(errorStatus, response, values,  rows) {
 	}
 };
 
-router.post('/user', (req, res) => {
+router.get('/user', (req, res) => {
 	if (typeof req.body.name !== 'undefined' && typeof req.body.password !== 'undefined') {
 		bd.query('SELECT * FROM User WHERE name = ? AND password=? ',[req.body.name, req.body.password], (err,result) => {
 			let value = [];
@@ -32,12 +33,21 @@ router.post('/user', (req, res) => {
 	}
 });
 
-router.patch('/:id', (req, res) => {
-	let iduser = req.params.id;
-	bd.query('UPDATE User SET name=?, password=?, mail=?, salt=? WHERE id=?',[req.body.name, req.body.password,req.body.mail, req.body.salt, iduser], (err, cols) => {
-		let values = [];
-		treatment(err,res,values, "success");
 
+router.patch('/user/:id', (req, res) => {
+	let iduser = req.params.id;
+	bcrypt.hash(req.body.password, saltRounds, (err, password) => {
+		if (!err) {
+				bd.query("UPDATE User SET name=?, password=?, mail=? WHERE id=? ",[req.body.name,password,req.body.email, iduser], (error, result) => {
+						if(error) throw error;
+						else{
+							let values = [];
+							treatment(err, res, values, "success");
+						}
+				});
+				res.send({ error: false });
+		} else
+				res.send({ error: err });
 	});
 });
 
