95258a58f5633deb131b365a2abc8915b50cce5b
hkarom <karom@localhost.localdomain>
2017-12-10 16:35:15 +0100

    config database travis correction

--- src/back-src/databaseConnect.js
+++ src/back-src/databaseConnect.js
@@ -6,7 +6,17 @@ console.log(process.env.DB_SERVER_HOST,
 	process.env.DB_SERVER_PASSWORD,
 	process.env.DB_NAME);
 
-let bd = mysql.createConnection({
+let bd;
+if(process.env.NODE_ENV == 'TEST')
+bd = mysql.createConnection({
+	host:		process.env.MYSQL_HOST,
+	port:		process.env.DB_SERVER_PORT,
+	user:		process.env.MYSQL_USER,
+	password:	process.env.MYSQL_PASSWORD,
+	database:	process.env.MYSQL_DATABASE
+});
+else
+bd = mysql.createConnection({
 	host:		process.env.DB_SERVER_HOST,
 	port:		process.env.DB_SERVER_PORT,
 	user:		process.env.DB_SERVER_USER,
