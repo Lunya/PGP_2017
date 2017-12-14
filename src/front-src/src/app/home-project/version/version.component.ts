bec820ce586e8d87f7965c55daeb09a242954a6a
GaelTM1AL <gaeltrottier@gmail.com>
2017-12-14 22:13:50 +0100

    fixVersion + maj routes.md

--- src/front-src/src/app/home-project/version/version.component.ts
+++ src/front-src/src/app/home-project/version/version.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit, ElementRef, NgZone, Input } from '@angular/core';
 import { Project } from '../../objects/Project';
 import { Version } from '../../objects/Version';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { HttpClient } from '@angular/common/http';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AuthService } from '../../services/auth.service';
 
 const urlVersion = 'http://localhost:3000/api/version';
 
@@ -18,6 +18,7 @@ export class VersionComponent implements OnInit {
   public version : Version;
   public versionTableView = false;
   private versionList = [];
+  private previousVersion = new Array<Version>();
 
   @Input('project')
   public project: Project;
@@ -25,7 +26,8 @@ export class VersionComponent implements OnInit {
   constructor(
     private el: ElementRef,
     private http: HttpClient,
-    private modalService: NgbModal
+    private modalService: NgbModal,
+    private auth: AuthService
   ) { }
 
   ngOnInit() {
@@ -47,4 +49,71 @@ export class VersionComponent implements OnInit {
 		this.versionTableView = value;
 	}
 
+  onDeleteVersionRow(ligne) {
+		const urlRequest = urlVersion + '/' + this.project.id + '/' + ligne['id'];
+		let headers = new HttpHeaders();
+		headers = this.auth.addAuthHeader(headers);
+		this.http.delete(urlRequest, { headers: headers })
+			.subscribe((result: any) => {
+				if (result.error) {
+					console.log(result);
+				} else {
+					const i = this.versionList.indexOf(ligne);
+					this.versionList.splice(i, 1);
+				}
+			}, err => {
+				console.log(err);
+			});
+	}
+
+  onEditVersionRow(ligne) {
+		const tr_id = '#Version' + ligne['id'];
+		ligne['onEdit'] = true;
+		this.previousVersion.push(Object.assign(new Version(), ligne));
+		this.el.nativeElement.querySelector(tr_id).classList.add('table-info');
+		const tab = this.el.nativeElement.querySelectorAll(tr_id + ' .editable');
+		for (let i = 0; i < tab.length; ++i) {
+			tab[i].setAttribute('contenteditable', 'true');
+		}
+	}
+
+  onConfirmVersionRow(ligne) {
+		const tr_id = '#Version' + ligne['id'];
+		const urlRequest = urlVersion + '/' + this.project.id + '/' + ligne['id'];
+		let headers = new HttpHeaders();
+		headers = this.auth.addAuthHeader(headers);
+		this.http.patch(urlRequest, ligne, { headers: headers })
+			.subscribe((result: any) => {
+				if (result.error) {
+					console.log(result);
+				}
+			}, err => {
+				console.log(err);
+			});
+		const save = this.previousVersion.filter((version) => version.id === ligne.id)[0];
+		this.previousVersion.splice(this.previousVersion.indexOf(save), 1);
+		this.el.nativeElement.querySelector(tr_id).classList.remove('table-info');
+		const tab = this.el.nativeElement.querySelectorAll(tr_id + ' .editable');
+		for (let i = 0; i < tab.length; ++i) {
+			tab[i].setAttribute('contenteditable', 'false');
+		}
+		ligne['onEdit'] = false;
+	}
+
+  onBackVersionRow(ligne) {
+		const tr_id = '#Version' + ligne['id'];
+		const save = this.previousVersion.filter((version) => version.id === ligne.id)[0];
+		ligne['num_version_maj'] = save.numVersionMaj;
+		ligne['link_source'] = save.linkSource;
+		ligne['link_build'] = save.linkBuild;
+		ligne['link_test'] = save.linkTest;
+    ligne['link_doc'] = save.linkDoc;
+		ligne['onEdit'] = false;
+		this.previousVersion.splice(this.previousVersion.indexOf(save), 1);
+		this.el.nativeElement.querySelector(tr_id).classList.remove('table-info');
+		const tab = this.el.nativeElement.querySelectorAll(tr_id + ' .editable');
+		for (let i = 0; i < tab.length; ++i) {
+			tab[i].setAttribute('contenteditable', 'false');
+		}
+	}
 }
